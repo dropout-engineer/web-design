7ff27be147bd8bfbaefe12b193443fe2dacba3f7
Ankit <91594377+dropout-engineer@users.noreply.github.com>
2024-07-30 23:58:01 +0530

    practice

--- HTML CANVAS/script.js
+++ HTML CANVAS/script.js
@@ -14,5 +14,34 @@ c.moveTo(100, 300)
 c.lineTo(400, 100)
 c.lineTo(400, 200)
 c.lineTo(300, 100)
-c.strokeStyle = '#fa34a3';
+c.strokeStyle = '#fa34a3'; 
 c.stroke()
+
+c.beginPath();
+c.arc (500, 300, 30, 0, Math.PI *  2, false)
+c.strokeStyle = 'blue'
+c.stroke()
+
+const getRandomColor = () =>{
+    let text = "0123456788ABCDEF"
+    let color = "#"
+
+    for(let i = 0; i < 6; i++){
+        color += text[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+
+
+for(let i = 0; i < 10; i++){
+    let x = Math.random() * window.innerWidth
+    let y = Math.random() * window.innerHeight
+    c.beginPath();
+    c.arc(x, y, 30, 0, Math.PI * 2, false)
+    c.strokeStyle = getRandomColor()
+    c.stroke()
+}
+
+// console.log(Math.floor(Math.random() * 16)) 
+
+
