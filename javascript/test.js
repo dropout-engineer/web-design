e1834e1b7154e483bc0cb65a2c137a5a5db492ab
Ankit <91594377+dropout-engineer@users.noreply.github.com>
2024-09-20 21:12:13 +0530

    practice

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,28 @@
-sddd
+const banana = {
+  name: 'banana',  quantity: 1, price: 1.95
+}
+
+const apple = {
+  name: 'apple', quantity: 1, price: 1.45
+}
+
+const candy = {
+  name: 'candy', quantity: 1, price: 3.50
+}
+
+const store = {
+  storeNumber: 5, locationCity: 'Milan', locationCountry: 'Italy', products: [banana, apple, candy,]
+}
+
+
+// console.log(store);
+console.log(store.products);
+
+console.log(store.products[2]);
+store.products[0].price = 1.75;
+
+console.log('-------------*-----------');
+
+
+console.log(store);
+console.log(banana);
